75a9c48f522a9e174881c9d8dc30f07f09d5dfb8
yuli.kamakura <barnett.yuxiang@gmail.com>
2024-12-17 12:25:43 +0800

    Initial Commit

---
+++ FeedFlow/FeedFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFlowApp.swift
+//  FeedFlow
+//
+//  Created by yuxiang on 2024/12/17.
+//
+
+import SwiftUI
+
+@main
+struct FeedFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FeedFlowTests/FeedFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFlowTests.swift
+//  FeedFlowTests
+//
+//  Created by yuxiang on 2024/12/17.
+//
+
+import Testing
+@testable import FeedFlow
+
+struct FeedFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
